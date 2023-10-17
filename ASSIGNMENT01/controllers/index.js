33007174c1db13623ac0cda65075d83b5cee22de
harvinder singh <harvinder.uppal10@icloud.com>
2023-10-16 23:08:31 -0400

    json files

--- ASSIGNMENT01/controllers/index.js
+++ ASSIGNMENT01/controllers/index.js
@@ -1,29 +1,26 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Set the view engine to Handlebars
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs');
 
 // Serve static files from the 'public' directory
 app.use(express.static(__dirname + '/public'));
 
-// Define routes for each page
-app.get('/', (req, res) => {
-    res.render('home', { title: 'Home' });
-});
+// Import route files
+const homeRoute = require('./routes/home');
+const aboutRoute = require('./routes/about');
+const projectsRoute = require('./routes/projects');
+const contactRoute = require('./routes/contact');
 
-app.get('/about', (req, res) => {
-    res.render('about', { title: 'About Me' });
-});
-
-app.get('/projects', (req, res) => {
-    res.render('projects', { title: 'Projects' });
-});
-
-app.get('/contact', (req, res) => {
-    res.render('contact', { title: 'Contact Me' });
-});
+// Use route files
+app.use('/', homeRoute);
+app.use('/about', aboutRoute);
+app.use('/projects', projectsRoute);
+app.use('/contact', contactRoute);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
